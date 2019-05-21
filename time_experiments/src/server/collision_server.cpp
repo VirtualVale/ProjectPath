cf5c3939745efad1abdaf95af6af17e850d05009
spannava <valentin.spannagl@student.uni-augsburg.de>
2019-05-21 17:43:37 +0200

    collision_test created, geometric test successfull, chronogical test pending

--- time_experiments/src/server/collision_server.cpp
+++ time_experiments/src/server/collision_server.cpp
@@ -1,27 +1,21 @@
 #include "ros/ros.h"
 #include "time_experiments/collisionsim.h"
 
+int checkPath_geometric(nav_msgs::Path superior, nav_msgs::Path inferior);
+bool checkPath_chronologic(int collisionPose, nav_msgs::Path superior, nav_msgs::Path inferior);
+
+//checks collision of two paths in a geometric and chronological manner, returns true if there is a collision
 bool simulateCollision(time_experiments::collisionsim::Request &req, time_experiments::collisionsim::Response &res){
     
     if(req.superior.poses.empty() || req.inferior.poses.empty())return false;
-    
-    for(int i=0; i<req.inferior.poses.size();i++){  
-        for(int j=0; j<req.superior.poses.size();j++){
 
-            double euclideanDistance = sqrt(pow((req.inferior.poses[i].pose.position.x-req.superior.poses[j].pose.position.x),2)+pow((req.inferior.poses[i].pose.position.y-req.superior.poses[j].pose.position.y),2));
-            
-            if(euclideanDistance<0.5){
-                ROS_INFO("Collision detected.");
-                res.collision.header.stamp = ros::Time::now();
-                res.collision.pose = req.inferior.poses[i];
-                res.collision.collision = true;
-                return true;
-            }else{//TODO necessary?
-            }
-        }
+    int poseID = checkPath_geometric(req.superior, req.inferior);
+    if(poseID >= 0){
+        if(checkPath_chronologic(poseID, req.superior, req.inferior))return false; //paths colliding but the duration between the motions is enough
     }
 
-    res.collision.collision = false;
+    res.collision.header.stamp = ros::Time::now();
+    res.collision.pose = req.inferior.poses[poseID].pose;
     return true;
 }
 
@@ -38,3 +32,37 @@ int main(int argc, char **argv)
 }
 
 //TODO functions checkPath_geometric, checkPath_chronologic
+
+//geometric Check for collisions
+int checkPath_geometric(nav_msgs::Path superior, nav_msgs::Path inferior)
+{
+    for(int i=0; i<inferior.poses.size();i++){  
+        for(int j=0; j<superior.poses.size();j++){
+
+            double euclideanDistance = sqrt(pow((inferior.poses[i].pose.position.x-superior.poses[j].pose.position.x),2)+pow((inferior.poses[i].pose.position.y-superior.poses[j].pose.position.y),2));
+            
+            if(euclideanDistance<0.5){
+                ROS_INFO("Collision detected.");
+                return i;
+                //returns the pose where collision is possible (inferior path)
+            }
+        }
+    }
+    return -1;
+    //no collision
+}
+
+//chronological comparison of poses 
+bool checkPath_chronologic(int collisionPose, nav_msgs::Path superior, nav_msgs::Path inferior){
+ 
+    int timeBtwPoses = superior.poses[collisionPose].header.stamp.sec - inferior.poses[collisionPose].header.stamp.sec; 
+    
+    if(timeBtwPoses > 500){
+        ROS_INFO("Duration btw Poses is enough.Go!");
+        return true;
+    } else {
+        ROS_INFO("Can't go there. We need a new plan.");
+        return false;
+    }
+    
+}
--- time_experiments/src/tests/collision_test.cpp
+++ time_experiments/src/tests/collision_test.cpp
@@ -1,13 +1,109 @@
 #include "ros/ros.h"
 #include "time_experiments/collisionsim.h"
+#include "time_experiments/timesim.h"
+#include "time_experiments/pathsim.h"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "collision_test");
     ros::NodeHandle n;
 
-    ros::ServiceClient client = n.serviceClient<time_experiments::collisionsim>("collisionsim");
-    time_experiments::collisionsim srv;
+    //client for collision detection: csrv
+    ros::ServiceClient cclient = n.serviceClient<time_experiments::collisionsim>("collisionsim");
+    time_experiments::collisionsim csrv;
 
-    ros::Publisher collision_pub = n.advertise<bool>("collision_pub", 1000);
-    //TODO has to be based on the path_server
+    //client for timestamping: tsrv
+    ros::ServiceClient tclient = n.serviceClient<time_experiments::timesim>("timesim");
+    time_experiments::timesim tsrv;
+
+    //client for pathplanning
+    ros::ServiceClient pclient = n.serviceClient<time_experiments::pathsim>("pathsim");
+    time_experiments::pathsim psrv;
+
+    //Creating the first path
+    nav_msgs::Path path1;
+
+    psrv.request.start.header.frame_id = "map";
+    psrv.request.start.header.stamp = ros::Time::now();
+    psrv.request.start.pose.position.x = -2.0;
+    psrv.request.start.pose.position.y = 1.0;
+    psrv.request.start.pose.orientation.w = 1.0;
+
+    psrv.request.goal.header.frame_id = "map";
+    psrv.request.goal.header.stamp = ros::Time::now();
+    psrv.request.goal.pose.position.x = 6.0;
+    psrv.request.goal.pose.position.y = -5.0;
+    psrv.request.goal.pose.orientation.w = 1.0;
+
+    if(pclient.call(psrv)){
+        path1 = psrv.response.path;
+        ROS_INFO("Path (1) size: %lu", psrv.response.path.poses.size());
+    } else {
+        ROS_INFO("Pathplanning 1 failed!");
+    }
+
+    //Creating a second path
+    nav_msgs::Path path2;
+
+    psrv.request.start.header.stamp = ros::Time::now();
+    psrv.request.start.pose.position.x = 6.0;
+    psrv.request.start.pose.position.y = 4.0;
+    psrv.request.start.pose.orientation.w = 1.0;
+
+
+    psrv.request.goal.header.stamp = ros::Time::now();
+    psrv.request.goal.pose.position.x = -4.0;
+    psrv.request.goal.pose.position.y = 4.0;
+    psrv.request.goal.pose.orientation.w = 1.0;
+
+    if(pclient.call(psrv)){
+        path2 = psrv.response.path;
+        ROS_INFO("Path (2) size: %lu", psrv.response.path.poses.size());
+    } else {
+        ROS_INFO("Pathplanning 2 failed!");
+    }
+
+    tsrv.request.original_path = path1;
+    tsrv.request.average_velocity = 2.0;
+
+    if(tclient.call(tsrv)){
+        path1 = tsrv.response.timesim_path;
+        ROS_INFO("Path (1) last time: %i", tsrv.response.timesim_path.poses[4].header.stamp.sec);
+    } else {
+      ROS_INFO("Timestamping 1 failed!");
+      return 1;
+    }
+
+    tsrv.request.original_path = path2;
+    tsrv.request.average_velocity = 1.0;
+
+    if(tclient.call(tsrv)){
+        path2 = tsrv.response.timesim_path;
+        ROS_INFO("Path (2) last time: %i", tsrv.response.timesim_path.poses[4].header.stamp.sec);
+    } else {
+      ROS_INFO("Timestamping 2 failed!");
+      return 1;
+    }
+
+    csrv.request.superior = path1;
+    csrv.request.inferior = path2;
+
+    if(cclient.call(csrv)){
+        ROS_INFO("Collision at Coordinates: %f, %f", csrv.response.collision.pose.position.x, csrv.response.collision.pose.position.y);
+    } else {
+        ROS_INFO("Paths do not collide!");
+        return 1;
+    }
+
+    ros::Publisher path_pub = n.advertise<nav_msgs::Path>("path", 1000);
+    ros::Publisher path2_pub = n.advertise<nav_msgs::Path>("path2", 1000);
+    ros::Publisher collision_pub = n.advertise<geometry_msgs::PoseStamped>("collision", 1000);
+    
+    while(ros::ok()){
+        path_pub.publish(path1);
+        path2_pub.publish(path2);
+        csrv.response.collision.header.frame_id = "map"; //TODO remove
+        collision_pub.publish(csrv.response.collision);
+    }
+    return 0;
+}
