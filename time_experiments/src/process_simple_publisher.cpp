9fcd7197e17c100638ff2e22a971fd9f2a3429c6
Valentin <valentin.spannagl@student.uni-augsburg.de>
2019-07-15 23:07:48 +0200

    time publisher updated, basic structure for rqt plugin has to be debugged

--- time_experiments/src/process_simple_publisher.cpp
+++ time_experiments/src/process_simple_publisher.cpp
@@ -10,15 +10,15 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
   ros::Publisher timer_pub = n.advertise<std_msgs::Time>("timer", 1000);
-  ros::Rate loop_rate(2);
+  ros::Rate loop_rate(60);
   std_msgs::Time time_count;
 
   while (ros::ok())
   {
     
-    for(int i=0; i<50; i++){
+    for(int i=0; i<3000; i++){
             
-            time_count.data.sec = 100 + i;
+            time_count.data = ros::Time(100 + i*0.01);
             timer_pub.publish(time_count);
             ros::spinOnce();
             loop_rate.sleep();
