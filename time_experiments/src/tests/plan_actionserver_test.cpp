f04c2e11e6495db1913e792348de42e897c0e5b7
spannava <valentin.spannagl@student.uni-augsburg.de>
2019-07-19 14:02:55 +0200

    Plan.msg file added, publishing the plan is the next goal

--- time_experiments/src/tests/plan_actionserver_test.cpp
+++ time_experiments/src/tests/plan_actionserver_test.cpp
@@ -54,6 +54,24 @@ int main (int argc, char **argv)
   else
     ROS_INFO("Action did not finish before the time out.");
 
+  goal.resource_number = 2;
+  goal.goal.pose.position.x = 6;
+  goal.goal.pose.position.y = 5.5;
+  goal.start_time.data = ros::Time(110);
+
+  ac.sendGoal(goal);
+
+  //wait for the action to return
+  finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+  if (finished_before_timeout)
+  {
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("Action finished: %s",state.toString().c_str());
+  }
+  else
+    ROS_INFO("Action did not finish before the time out.");
+
   //exit
   return 0;
 }
