ba4d07875424276f7e9be64f4f29f3530119564c
Valentin <valentin.spannagl@student.uni-augsburg.de>
2019-08-10 16:24:13 +0200

    actionserver executeCB updated new function has to be implemented and collisionchecking

--- chronos/src/actionserver/plan_actionserver.cpp
+++ chronos/src/actionserver/plan_actionserver.cpp
@@ -6,6 +6,7 @@
 #include "chronos/time_service.h"
 #include "chronos/collision_service.h"
 #include <chronos/PTSAction.h>
+#include <geometry_msgs/Pose.h>
 
 bool occupiedBool(ros::Time time, std::vector<nav_msgs::Path> resource_plan);
 
@@ -59,38 +60,60 @@ public:
         ros::Time startTime = goal->start_time.data;
         if(occupiedBool(startTime, plan[resource])){
             ROS_INFO("ERROR: Resource is busy at this start time.")
+            return false;
         }
 
         //START POSITION DETERMINATION
-        double startX;
-        double startY; 
+        geometry_msgs::Pose start; 
 
         if(plan[resource].size() == 0 || startTime < plan[resource].front()) // no paths registered
         {
             switch (resource)
             {
                 case 0:
-                startX = 0;
-                startY = 0;
+                start.position.x = 0;
+                start.position.y = 0;
                 break;
                 case 1:
-                startX = 0;
-                startY = 1;
+                start.position.x = 0;
+                start.position.y = 1;
                 break;
                 case 2:
-                startX = 0;
-                startY = 2;
+                start.position.x = 0;
+                start.position.y = 2;
                 break;
                 default:
                 ROS_INFO("Error: Invalid resource number");
             }
         } else {
-
+            int pathId = pathAtTime(startTime, plan[resource]);
+            //time is between zero time and last path
+            start.position.x = plan[resource][pathId].poses.back().pose.position.x;
+            start.position.y = plan[resource][pathId].poses.back().pose.position.y;
         }
-        
+
+        //GOAL POSITION
+        geometry_msgs::Pose goal;
+        goal.position.x = goal->goal.pose.position.x;
+        goal.position.y = goal->goal.pose.position.y;
+        //TODO check the input
+
+        //COLLISIONCHECKING
+        //TODO call create Path and look for the chronological order where to insert the created path
+        as_.setSucceeded(result_);
     }
 }
 
+int int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "PTS");
+    PTSAction PTS("PTS");
+    ros::NodeHandle n;
+    ros::spin();
+    return 0;
+}
+
+//calculates if a timestamp lies within a already registered path
 bool occupiedBool(ros::Time time, std::vector<nav_msgs::Path> resource_plan)
 {
     for(int i=0; i<resource_plan.size(); i++)
@@ -103,5 +126,56 @@ bool occupiedBool(ros::Time time, std::vector<nav_msgs::Path> resource_plan)
     return false;
 }
 
+//returns the Id of the path the translated timestamp lies within
+int pathAtTime(ros::Time currentTime, std::vector<nav_msgs::Path> plan){
 
+   int pathID = 0;
+   double diff, optDiff = 100.00;
+   for(int i=0; i<plan.size(); ++i){
+     for(int j=0; j<plan[i].poses.size(); ++j){
+       diff = abs(currentTime.toSec() - plan[i].poses[j].header.stamp.toSec());
+       if(diff < optDiff){
+         optDiff = diff;
+         pathID = i;
+       }
+     }
+     //ROS_INFO("for-Loop(currentPath) i = %i, optDiff = %lf, diff = %lf, pathID = %i", i, optDiff, diff, pathID);
+   }
+   return pathID;
+}
 
+//pathplanning and timestamping, RETURN the plan
+nav_msgs::Path createPath( geometry_msgs::Pose start, geometry_msgs::Pose goal, ros::Time startTime, ros::ServiceClient path_client, ros::ServiceClient time_client)
+{
+    //path service call
+    chronos::path_service psrv;
+    psrv.request.start.header.frame_id = "map";
+    psrv.request.start.pose.position.x = start.position.x;
+    psrv.request.start.pose.position.y = start.position.y;
+    psrv.request.start.pose.orientation.w = 1.0;
+    psrv.request.goal.header.frame_id = "map";
+    psrv.request.goal.pose.position.x = goal.position.x;
+    psrv.request.goal.pose.position.y = goal.position.y;
+    psrv.request.goal.pose.orientation.w = 1.0;
+    if(path_client.call(psrv))
+    {
+        ROS_INFO("Path size: %lu", psrv.response.path.poses.size());
+    } else {
+        ROS_INFO("Path creation failed!");
+        return NULL;
+    }
+
+    //time service call
+    chronos::time_service tsrv;
+    tsrv.request.path = psrv.response.path;
+    tsrv.request.path.poses.front().header.stamp = time;
+    tsrv.request.average_velocity = 0.5;
+    if(time_client.call(tsrv)){
+      ROS_INFO("start time: %.2lf, end time: %.2lf", tsrv.response.timesim_path.poses.front().header.stamp.toSec(), tsrv.response.timesim_path.poses.back().header.stamp.toSec());
+    } else {
+      ROS_INFO("Timestamping failed!");
+      return NULL;
+    }    
+
+    return tsrv.response.path_timestamped;
+}
