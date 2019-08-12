f36a89acc35973ba694d36c37e7301e97414971c
spannava <valentin.spannagl@student.uni-augsburg.de>
2019-08-12 10:52:36 +0200

    PTS_actionserver collisionchecking and chronological insertion added, next step is the test of all functionalities of the actionserver

--- chronos/src/actionserver/PTS_actionserver.cpp
+++ chronos/src/actionserver/PTS_actionserver.cpp
@@ -10,6 +10,8 @@
 
 bool occupiedBool(ros::Time time, std::vector<nav_msgs::Path> resource_plan);
 nav_msgs::Path createPath( geometry_msgs::Pose start, geometry_msgs::Pose goal, ros::Time startTime, ros::ServiceClient path_client, ros::ServiceClient time_client);
+int pathAtTime(ros::Time currentTime, std::vector<nav_msgs::Path> plan);
+std::vector<nav_msgs::Path> insertPath(nav_msgs::Path path, std::vector<nav_msgs::Path> plan);
 
 
 class PTSAction
@@ -18,13 +20,13 @@ protected:
 
     ros::NodeHandle n;
     actionlib::SimpleActionServer<chronos::PTSAction> as_;
-    std::string action_name;
+    std::string action_name_;
     //message for feedback /result
     chronos::PTSFeedback feedback_;
     chronos::PTSResult result_;
 
     //one plan to rule them all
-    std::vector<std::vector<nav_msgs::Path> > plan[99];
+    std::vector<nav_msgs::Path>  plan[99];
 
     //clients for the required server
     ros::ServiceClient path_client;
@@ -47,7 +49,7 @@ public:
     {
     }
 
-    void executeCB(const chronos::PTSGoalConstPtr &goal)
+    bool executeCB(const chronos::PTSGoalConstPtr &goal)
     {
         //RESOURCE
         //Resources 1,2,3 are mapped to 0,1,2 for computation reasons
@@ -61,28 +63,28 @@ public:
         //START TIME
         ros::Time startTime = goal->start_time.data;
         if(occupiedBool(startTime, plan[resource])){
-            ROS_INFO("ERROR: Resource is busy at this start time.")
+            ROS_INFO("ERROR: Resource is busy at this start time.");
             return false;
         }
 
         //START POSITION DETERMINATION
-        geometry_msgs::Pose start; 
+        geometry_msgs::Pose startPose; 
 
-        if(plan[resource].size() == 0 || startTime < plan[resource].front()) // no paths registered
+        if(plan[resource].size() == 0 || startTime < plan[resource].front().header.stamp) // no paths registered
         {
             switch (resource)
             {
                 case 0:
-                start.position.x = 0;
-                start.position.y = 0;
+                startPose.position.x = 0;
+                startPose.position.y = 0;
                 break;
                 case 1:
-                start.position.x = 0;
-                start.position.y = 1;
+                startPose.position.x = 0;
+                startPose.position.y = 1;
                 break;
                 case 2:
-                start.position.x = 0;
-                start.position.y = 2;
+                startPose.position.x = 0;
+                startPose.position.y = 2;
                 break;
                 default:
                 ROS_INFO("Error: Start position for resource number not implemented");
@@ -91,23 +93,72 @@ public:
         } else {
             int pathId = pathAtTime(startTime, plan[resource]);
             //time is between zero time and last path
-            start.position.x = plan[resource][pathId].poses.back().pose.position.x;
-            start.position.y = plan[resource][pathId].poses.back().pose.position.y;
+            startPose.position.x = plan[resource][pathId].poses.back().pose.position.x;
+            startPose.position.y = plan[resource][pathId].poses.back().pose.position.y;
         }
 
         //GOAL POSITION
-        geometry_msgs::Pose goal;
-        goal.position.x = goal->goal.pose.position.x;
-        goal.position.y = goal->goal.pose.position.y;
+        geometry_msgs::Pose goalPose;
+        goalPose.position.x = goal->goal.pose.position.x;
+        goalPose.position.y = goal->goal.pose.position.y;
         //TODO check the input
 
+        //PATHCREATION
+        nav_msgs::Path createdPath;
+        createdPath = createPath(startPose, goalPose, startTime, path_client, time_client);
+        
         //COLLISIONCHECKING
-        //TODO call create Path and look for the chronological order where to insert the created path
-        as_.setSucceeded(result_);
-    }
-}
+        chronos::collision_service csrv;
+        csrv.request.superior = createdPath;
+        
+        //check possible collisions with paths from bigger resource numbers
+        for(int i=resource+1; i<99; i++)
+        {
+            for(int j=0; j<plan[i].size(); j++)
+            {
+                if(plan[i].empty())break;
+                csrv.request.inferior = plan[i][j];
+                if(collision_client.call(csrv))
+                {
+                    ROS_INFO("Collisions #: %lu", csrv.response.collisionTimes.data.size());
+                }
+                else
+                {
+                    ROS_INFO("ERROR: Collisionchecking failed.");
+                }
+            }
+        }
 
-int int main(int argc, char *argv[])
+        //check possible collision with paths with smaller resource numbers
+        for(int i=resource-1; i>=0; i--)
+        {
+            for(int j=0; j<plan[i].size(); j++)
+            {
+                if(plan[i].empty())break;
+                csrv.request.inferior = plan[i][j];
+                if(collision_client.call(csrv))
+                {
+                    ROS_INFO("Collisions #: %lu", csrv.response.collisionTimes.data.size());
+                }
+                else
+                {
+                    ROS_INFO("ERROR: Collisionchecking failed.");
+                }
+            }
+        }
+
+        //REACTION TO POSSIBLE COLLISIONS
+
+        //ADDING PATH TO PLAN
+        plan[resource] = insertPath(createdPath, plan[resource]);
+
+
+        as_.setSucceeded(result_);
+        return true;
+    }
+};
+
+int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "PTS");
     PTSAction PTS("PTS");
@@ -121,7 +172,7 @@ bool occupiedBool(ros::Time time, std::vector<nav_msgs::Path> resource_plan)
 {
     for(int i=0; i<resource_plan.size(); i++)
     {
-        if(startTime >= resource_plan[i].poses.front().header.stamp && startTime <= resource_plan[i].poses.back().header.stamp)
+        if(time >= resource_plan[i].poses.front().header.stamp && time <= resource_plan[i].poses.back().header.stamp)
         {
             return true;
         }
@@ -130,8 +181,8 @@ bool occupiedBool(ros::Time time, std::vector<nav_msgs::Path> resource_plan)
 }
 
 //returns the Id of the path the translated timestamp lies within
-int pathAtTime(ros::Time currentTime, std::vector<nav_msgs::Path> plan){
-
+int pathAtTime(ros::Time currentTime, std::vector<nav_msgs::Path> plan)
+{
    int pathID = 0;
    double diff, optDiff = 100.00;
    for(int i=0; i<plan.size(); ++i){
@@ -165,20 +216,41 @@ nav_msgs::Path createPath( geometry_msgs::Pose start, geometry_msgs::Pose goal, 
         ROS_INFO("Path size: %lu", psrv.response.path.poses.size());
     } else {
         ROS_INFO("Path creation failed!");
-        return NULL;
     }
 
     //time service call
     chronos::time_service tsrv;
     tsrv.request.path = psrv.response.path;
-    tsrv.request.path.poses.front().header.stamp = time;
+    tsrv.request.path.poses.front().header.stamp = startTime;
     tsrv.request.average_velocity = 0.5;
     if(time_client.call(tsrv)){
-      ROS_INFO("start time: %.2lf, end time: %.2lf", tsrv.response.timesim_path.poses.front().header.stamp.toSec(), tsrv.response.timesim_path.poses.back().header.stamp.toSec());
+      ROS_INFO("start time: %.2lf, end time: %.2lf", tsrv.response.path_timestamped.poses.front().header.stamp.toSec(), tsrv.response.path_timestamped.poses.back().header.stamp.toSec());
     } else {
       ROS_INFO("Timestamping failed!");
-      return NULL;
     }    
 
     return tsrv.response.path_timestamped;
+}
+
+std::vector<nav_msgs::Path> insertPath(nav_msgs::Path createdPath, std::vector<nav_msgs::Path> plan)
+{
+        if(plan.empty())
+        {
+            plan.push_back(createdPath);
+            return plan;
+        }
+        else
+        {
+            std::vector<nav_msgs::Path>::iterator it = plan.begin();
+            for(int i=0; i<plan.size(); i++)
+            {
+                if(plan[i].poses.front().header.stamp > createdPath.poses.front().header.stamp)
+                {
+                    plan.insert(it+i, createdPath);
+                    return plan;
+                }
+            }
+            ROS_INFO("ERROR: path insertion found no correct place");
+            return plan;
+        }
 }
