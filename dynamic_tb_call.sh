69e2b61e00b00dbd0ca2208d9e504fabe43e5454
valo <valentin.spannagl@student.uni-augsburg.de>
2020-03-24 09:14:34 +0100

    script with loop finished, next step is the ros content

--- dynamic_tb_call.sh
+++ dynamic_tb_call.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-roslaunch path_planning_system setup.launch
+number=$(zenity --entry --text "how many robots" --entry-text "1" --title "number robots" 2> /dev/null) 
+echo "$number"
+zenity --info --text "Valo spawns $number robots!" 2> /dev/null
+counter=1
+while [ $counter -le $number ]
+do
+    echo $counter
+    ((counter++))
+done
+
+echo All done
+#roslaunch path_planning_system setup.launch
