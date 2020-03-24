48828c0cf44517403d2d58273adad2017fb6712a
valo <valentin.spannagl@student.uni-augsburg.de>
2020-03-24 13:27:55 +0100

    bash script almost finished, jobs are automatically started in the background and afterwards killed

--- dynamic_tb_call.sh
+++ dynamic_tb_call.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-number=$(zenity --entry --text "how many robots" --entry-text "1" --title "number robots" 2> /dev/null) 
+x_pos=(0 0 0 0 0 0 0 0 0 0)
+y_pos=(0 0 1 2 3 4 5 6 7 8)
+number=$(zenity --entry --text "How many robots should spawn in the simulation?
+(Valid are integers between 1 and 10)" --entry-text "1" --title "Number of robots" 2> /dev/null) 
 echo "$number"
+roslaunch path_planning_system dynamic_gazebo_map_rviz.launch &
+if [ $number -le 0 -o $number -ge 10 ]
+    then
+        echo "Wrong input!"
+        exit  255
+fi
 zenity --info --text "Valo spawns $number robots!" 2> /dev/null
 counter=1
 while [ $counter -le $number ]
 do
     echo $counter
+    echo ${y_pos[$counter]}
+    roslaunch path_planning_system single_tb_call.launch tb_number:=$counter y_pos:=${y_pos[$counter]} &
     ((counter++))
 done
-
 echo All done
+echo press k to shutdown
+kill_command="o"
+while [ $kill_command != "k" ]
+do
+    read kill_command
+done
+echo kill
+kill $(jobs -p)
 #roslaunch path_planning_system setup.launch
