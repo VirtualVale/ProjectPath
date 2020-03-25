eb6b9aaaa690d6e8f51587f6205638268e8cb356
valo <valentin.spannagl@student.uni-augsburg.de>
2020-03-25 10:25:51 +0100

    SIGINT with trap implemented, next step is validating the robot spawn and the related ros topics

--- dynamic_tb_call.sh
+++ dynamic_tb_call.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap "echo haha,was" SIGINT SIGTERM
 x_pos=(0 0 0 0 0 0 0 0 0 0)
 y_pos=(0 0 1 2 3 4 5 6 7 8)
 number=$(zenity --entry --text "How many robots should spawn in the simulation?
@@ -9,7 +10,7 @@ if [ $number -le 0 -o $number -ge 10 ]
         echo "Wrong input!"
         exit  255
 fi
-roslaunch path_planning_system dynamic_gazebo_map_rviz.launch &
+roslaunch -v path_planning_system dynamic_gazebo_map_rviz.launch &
 sleep 5s
 if ! zenity --warning --text "$number robots are spawned!" 2> /dev/null; then
   kill $(jobs -p)
@@ -18,12 +19,10 @@ fi
 counter=1
 while [ $counter -le $number ]
 do
-    echo $counter
-    echo ${y_pos[$counter]}
-    roslaunch path_planning_system single_tb_call.launch tb_number:=$counter y_pos:=${y_pos[$counter]} &
+    roslaunch -v path_planning_system dynamic_single_robot.launch tb_number:=$counter y_pos:=${y_pos[$counter]} &
     ((counter++))
 done
-echo All done
+sleep 5s
 echo press k to shutdown
 kill_command="o"
 while [ $kill_command != "k" ]
